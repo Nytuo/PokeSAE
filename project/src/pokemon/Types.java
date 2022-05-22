2b3ef380de5838c63f50822c847a9461443ebf46
Arnaud BEUX <34894524+Nytuo@users.noreply.github.com>
2022-05-22 17:39:24 +0200

    JavaDoc Types
    Mise en place de la JavaDoc pour la classe Types

--- project/src/pokemon/Types.java
+++ project/src/pokemon/Types.java
@@ -1,12 +1,29 @@
 package pokemon;
 
-public class Types implements interfaces.IType {
+/**
+ * @author Arnaud BEUX
+ * Une classe qui permet de créer un type pour un Pokemon ou une capacité.
+ * Exemple : Plante, Feu, Glace, etc...
+ */
+public final class Types implements interfaces.IType {
 
+    /**
+     * Le nom du type.
+     */
     private final String nom;
+
+    /**
+     * Constructeur de la classe Types
+     * @param nom String (Le nom du type).
+     */
     public Types(String nom) {
         this.nom = nom;
     }
 
+    /**
+     * Retourne le nom du type
+     * @return String nom. (Le nom du type associé)
+     */
     @Override
     public String getNom() {
         return this.nom;
