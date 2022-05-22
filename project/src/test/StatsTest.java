c3d0424af13b28b1854e09593024aa0704ba2b1b
Arnaud BEUX <34894524+Nytuo@users.noreply.github.com>
2022-05-22 17:58:56 +0200

    Commencement des testes
    - Debut des testes sur Stats et Types

---
+++ project/src/test/StatsTest.java
@@ -0,0 +1,49 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatsTest {
+
+    @Test
+    void getPV() {
+
+    }
+
+    @Test
+    void getForce() {
+    }
+
+    @Test
+    void getDefense() {
+    }
+
+    @Test
+    void getSpecial() {
+    }
+
+    @Test
+    void getVitesse() {
+    }
+
+    @Test
+    void setPV() {
+    }
+
+    @Test
+    void setForce() {
+    }
+
+    @Test
+    void setDefense() {
+    }
+
+    @Test
+    void setVitesse() {
+    }
+
+    @Test
+    void setSpecial() {
+    }
+}
