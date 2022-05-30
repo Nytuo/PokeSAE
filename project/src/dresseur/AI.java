a273ead42f1f2963c481e8605d947562c6c94837
Jules Mahe <104528820+ZaY-eZ@users.noreply.github.com>
2022-05-30 13:17:29 +0200

    Dresseur.java complété

--- project/src/dresseur/AI.java
+++ project/src/dresseur/AI.java
@@ -1,10 +1,47 @@
 package dresseur;
 
 import java.util.ArrayList;
+import java.util.Random;
+
+import interfaces.IAttaque;
+import interfaces.ICapacite;
+import interfaces.IPokemon;
+import interfaces.IStrategy;
+import pokemon.Capacite;
 import pokemon.Pokemon;
 
-public class AI extends Dresseur {
+/**
+ * Une stratégie est utilisée par les dresseurs non humains (IA) pour prendre les décisions 
+ * Un DresseurIA possède une référence sur une IStrategy à qui il délègue la prise de décision
+ * Un dresseur humain n'utilise pas IStrategy
+ * 
+ * Chaque méthode de IStrategy correspond à la méthode homonyme de IDresseur
+ */
+
+public class AI extends Dresseur implements IStrategy {
   public AI(String name, int level, ArrayList<Pokemon> pokemons) {
     super(name, level, pokemons);
   }
+
+@Override
+public IPokemon choisitCombattant() {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+@Override
+public IPokemon choisitCombattantContre(IPokemon pok) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+@Override
+public IAttaque choisitAttaque(IPokemon attaquant, IPokemon defenseur) {
+	Capacite[] capList =  (Capacite[]) attaquant.getCapacitesApprises(); // Récupère toutes les capacités de l'attaquant
+	Random r = new Random();
+	int numAttaque = r.nextInt((3 ) ) ;//Choisit un nombre aléatoire entre 0 et 3 
+	return capList[numAttaque]; 
+}
+  
+  
 }
--- project/src/dresseur/Dresseur.java
+++ project/src/dresseur/Dresseur.java
@@ -1,22 +1,87 @@
 package dresseur;
 
 import java.util.ArrayList;
+import java.util.Scanner;
+
+import interfaces.IAttaque;
+import interfaces.ICapacite;
+import interfaces.IDresseur;
+import interfaces.IPokemon;
+import pokemon.Capacite;
 import pokemon.Pokemon;
 
-public class Dresseur {
+public class Dresseur implements IDresseur {
   public String name;
-  public int level;
+  public int level = 0;
   public ArrayList<Pokemon> pokemons;
 
   public Dresseur(String name, int level, ArrayList<Pokemon> pokemons) {
+	  
     this.name = name;
-    this.level = level;
     this.pokemons = pokemons;
+    
+    for (Pokemon pok : pokemons) {
+    	this.level += pok.getNiveau();
+    }
   }
 
-  private void summonPokemon(Pokemon pokemon) {
-    pokemons.set(0, pokemon);
-  }
+@Override
+public void enseigne(IPokemon pok, ICapacite[] caps) {
+	// TODO Auto-generated method stub
+	
+}
 
-  private void ChooseAttack(Pokemon pokemon) {}
+@Override
+public void soigneRanch() {
+	for (Pokemon pok : pokemons) {
+		pok.soigne();
+	}
+	
+}
+
+@Override
+public IPokemon choisitCombattant() {
+	
+	return getPokemon(0);
+}
+
+@Override
+public IPokemon choisitCombattantContre(IPokemon pok) {
+	
+	return pok;
+}
+
+@Override
+public IAttaque choisitAttaque(IPokemon attaquant, IPokemon defenseur) {
+	
+	Capacite[] capList =  (Capacite[]) attaquant.getCapacitesApprises(); // Récupère toutes les capacités de l'attaquant
+	
+	Scanner scanner = new Scanner(System.in);
+
+	System.out.print("Choose the move to use\n" + attaquant.getCapacitesApprises());
+	String numAttaque = scanner.nextLine();
+	System.out.println();
+
+	scanner.close();
+	
+    return capList[Integer.valueOf(numAttaque)];
+}
+
+@Override
+public int getNiveau() {
+	
+	return this.level;
+}
+
+@Override
+public String getNom() {
+	
+	return this.name;
+}
+
+@Override
+public IPokemon getPokemon(int i) {
+	
+	return pokemons.get(i);
+}
 }
