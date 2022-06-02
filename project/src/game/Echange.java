1335915d1c22970ebdf83c967475ce14f2625f58
Jules Mahe <104528820+ZaY-eZ@users.noreply.github.com>
2022-06-02 17:47:49 +0200

    finition dresseur.java / début combat.java; tour.java et Echange.java

---
+++ project/src/game/Echange.java
@@ -0,0 +1,45 @@
+package game;
+
+import java.util.Scanner;
+
+import interfaces.IEchange;
+import interfaces.IPokemon;
+import pokemon.Capacite;
+
+public class Echange implements IEchange {
+
+	@Override
+	public int calculeDommage(IPokemon lanceur, IPokemon receveur) {
+
+		return 0;
+	}
+
+	@Override
+	public void utilise() {		
+	}
+
+	@Override
+	public void setPokemon(IPokemon pok) {
+				
+		
+	}
+
+	@Override
+	public IPokemon echangeCombattant() {
+
+		Capacite[] equipe =
+		        (Capacite[])
+		            attaquant.getCapacitesApprises(); // R�cup�re toutes les capacit�s de l'attaquant
+
+		    Scanner scanner = new Scanner(System.in);
+
+		    System.out.print("Choose your new pokemon\n" + equipe);
+		    String numAttaque = scanner.nextLine();
+		    System.out.println();
+
+		    scanner.close();
+		
+		return (IPokemon) this;
+	}
+
+}
---
+++ project/src/game/Tour.java
@@ -0,0 +1,22 @@
+package game;
+
+import interfaces.IAttaque;
+import interfaces.IPokemon;
+import interfaces.ITour;
+
+public class Tour implements ITour {
+
+	
+	
+	@Override
+	public void commence() {
+		
+		IPokemon pok1;
+		IAttaque atk1; 
+		IPokemon pok2; 
+		IAttaque atk2;
+		
+		
+	}
+
+}
