33cdd5c621b38861044a1538d35bc40930aff146
Arnaud BEUX <34894524+Nytuo@users.noreply.github.com>
2022-06-08 09:52:12 +0200

    Suppression de l'ancienne doc
    Le dossier étant mal nommé la nouvelle doc à était généré dans un autre dossier

--- doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"dresseur"},{"l":"game"},{"l":"interfaces"},{"l":"pokedex"},{"l":"pokemon"},{"l":"test"}];updateSearchResults();
