282cfee1a856c28547c27deb30c6d72a8ba23cad
Arnaud BEUX <34894524+Nytuo@users.noreply.github.com>
2022-06-10 09:25:58 +0200

    Interfaces
    - Passage aux interfaces commentés (non visible dans le JavaDoc)
    - Restructuration du packages des interfaces
    - Doc mis à jour pour le nouveau package

--- project/interfaces/interfaces/ITour.java
+++ project/interfaces/interfaces/ITour.java
@@ -1,15 +1,14 @@
 /**
  * Université Côte d'Azur IUT Côte d'Azur Département Informatique
  *
- * @date ICategory.java
+ * @date ITour.java
  */
 package interfaces;
 
 /**
  * @author Leo Donati
  */
-public interface ICategorie {
-  boolean isSpecial();
-
-  String getNom();
+public interface ITour {
+  /** Lance un tour de combat */
+  void commence();
 }
